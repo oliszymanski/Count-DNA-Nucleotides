b999d741d523346ab41895f29a37432708f5805d
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2021-07-24 15:23:43 +0200

    adding the function to count the percentage of given data

--- demo/nucleotide_count.py
+++ demo/nucleotide_count.py
@@ -24,19 +24,60 @@ class CountNucleotides:
 
 
 
-    def Count(self, s_file_path):
+    def count_percentage(self, ls_amounts):
 
-        ls_a = []       # lists for inserting data
-        ls_c = []
-        ls_g = []
-        ls_t = []
+        ls_percentages = []
+
+        all_data = sum(ls_amounts)
+
+
+        for element_num in ls_amounts:
+            percentage = element_num/all_data
+
+
+
+        return
+
+
+
+    def count_amount(self, s_file_path):
+
+        ls_a_amount = []       # lists for inserting data
+        ls_c_amount = []
+        ls_g_amount = []
+        ls_t_amount = []
+
+        ls_amount_amount = []
+
+
 
         ls_file = open(s_file_path, 'r')       # read the file in binary
 
         ls_file = self.convert_elements_to_string(ls_file)
 
 
+        for line in ls_file:
+            for element in line:
+
+                if (element == 'A'): ls_a_amount.append(element)       # find adenine
+
+                elif (element == 'C'): ls_c_amount.append(element)     # find cytosine
+
+                elif (element == 'G'): ls_g_amount.append(element)     # find guanine
+
+                elif (element == 'T'): ls_t_amount.append(element)     # find thymine
+
+
+        ls_amount_amount.append(len(ls_a_amount))
+        ls_amount_amount.append(len(ls_c_amount))
+        ls_amount_amount.append(len(ls_g_amount))
+        ls_amount_amount.append(len(ls_t_amount))
+
+
         return
+
+
+
 
 
 
