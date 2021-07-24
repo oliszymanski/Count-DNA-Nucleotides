3d7671308ec3e63e3f35dd15a6f3411ec52886f2
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2021-07-24 14:34:46 +0200

    add tests file for testing nucleotide_count.py class

---
+++ demo/nucleotide_count.py
@@ -0,0 +1,36 @@
+#================================================
+#   IMPORTS
+#================================================
+
+
+
+#================================================
+#   CLASSES
+#================================================
+
+class CountNucleotides:
+
+    def __init__(self):     # for initializing class
+
+        return
+
+
+
+    def Count(self, path : str, file_name : str):
+
+        ls_a = []       # lists for inserting data
+        ls_c = []
+        ls_g = []
+        ls_t = []
+
+
+        file_to_read = open(path + '/' + file_name, 'r')
+
+
+        return
+
+
+
+#================================================
+#   END OF FILE
+#================================================
