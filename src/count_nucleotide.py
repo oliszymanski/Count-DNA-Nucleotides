13609a59b468238cdd3f30d3e6314838c10867db
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2021-07-28 14:03:18 +0200

    add calculating functions

---
+++ src/count_nucleotide.py
@@ -0,0 +1,53 @@
+#================================================
+#   IMPORTS
+#================================================
+
+
+
+#================================================
+#   CLASSES
+#================================================
+
+class CountNucleotides:
+
+    def __init__(self):
+        '''Initializing class'''
+
+        return
+
+
+
+    def count_percentage(self):
+
+
+
+        return
+
+
+
+    def count_nucleotides(self, file_path):
+
+        ls_s_a = []
+        ls_s_c = []
+        ls_s_g = []
+        ls_s_t = []
+
+        ls_data = []
+
+
+
+
+        return ls_data
+
+
+
+
+    def count_percentage(self, ls_amount):
+
+
+
+        return
+
+#================================================
+#   END OF FILE
+#================================================
