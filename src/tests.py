0c854f740c81ac247823a2d806c7c2cae075b5fa
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2021-07-28 23:41:26 +0200

    adding global file path and running tests

--- src/tests.py
+++ src/tests.py
@@ -6,10 +6,25 @@ import count_nucleotide as cn
 
 
 
+#================================================
+#   GLOBALS
+#================================================
+
+s_file_path = "src-data/Acetobacter_pasterianus_dna.txt"        # main file path
+
+
 
 #================================================
 #   MAIN
 #================================================
+
+if (__name__ == '__main__'):
+
+    search_nucleotides = cn.CountNucleotides()          # create object
+
+    search_nucleotides.count_nucleotides(s_file_path)
+
+
 
 
 
