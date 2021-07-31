36f7d2252539780997fde714b6e1ea74770172fe
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2021-07-31 15:20:12 +0200

    testing escherichia genome

--- src/Main.py
+++ src/Main.py
@@ -11,7 +11,7 @@ import count_nucleotide as cn
 #================================================
 
 s_file_path = "src-data/Acetobacter_pasterianus_dna.txt"
-
+s_second_file_path = "src-data/Escherichia_phage.txt"
 
 
 #================================================
@@ -28,15 +28,23 @@ if (__name__ == '__main__'):
 
 
 
-    nucleotide_number = search_nucleotides.count_nucleotides(s_file_path)
-
+    nucleotide_number = search_nucleotides.count_nucleotides(s_file_path)       # acetobacter genome
     print("nucleotide_number =", nucleotide_number, "\n\n")
 
 
     nucleotide_percentage = search_nucleotides.count_nucleotide_percentage(s_file_path)
-    print("nucleotide_percentage =", nucleotide_percentage)
+    print("nucleotide_percentage =", nucleotide_percentage, "\n\n\n\n")
 
 
+
+
+
+    second_nucleotides_number = search_nucleotides.count_nucleotides(s_second_file_path)    # Escherichia genome
+    print("second_nucleotides_number =", second_nucleotides_number, "\n\n")
+
+
+    second_nucleotide_percentage = search_nucleotides.count_nucleotide_percentage(s_second_file_path)
+    print("second_nucleotides_number =", second_nucleotide_percentage, "\n\n\n\n")
 
 #================================================
 #   END OF FILE
