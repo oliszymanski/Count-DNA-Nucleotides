7b881e98b91658f51fd7fb1b26945563c0f3f2a3
oliszymanski <oliwier.szymanski112@gmail.com>
2021-10-17 18:54:38 +0200

    demo update: adding count_sequence() to demo

--- demo/nucleotide_count.py
+++ demo/nucleotide_count.py
@@ -73,7 +73,7 @@ class CountNucleotides:
                 elif (element == 'T'): ls_t_amount.append(element)     # find thymine
 
 
-        ls_data_amount.append(len(ls_a_amount))
+        ls_data_amount.append(len(ls_a_amount))     # adding the amount of adenine, guanine etc.
         ls_data_amount.append(len(ls_c_amount))
         ls_data_amount.append(len(ls_g_amount))
         ls_data_amount.append(len(ls_t_amount))
@@ -82,6 +82,32 @@ class CountNucleotides:
 
 
         return ls_data_amount, ls_percentage
+
+
+
+
+    def count_sequences(self, ls_meta : list, sequence):
+
+        """
+            input:
+                ls_meta :       meta data which theoretically contains the searched sequence,
+                sequence :      searched sequence;
+
+            output:
+
+        """
+
+        ls_second = []
+
+        for val in ls_meta:
+            if ( ls_meta[ ls_meta[val] : len(sequence) ] == sequence  ):        # checking if metadata contains the sequence (going from i-th element to )
+                ls_second.append(True)
+
+            else:
+                ls_second.append(False)
+
+
+        return  None
 
 
 
